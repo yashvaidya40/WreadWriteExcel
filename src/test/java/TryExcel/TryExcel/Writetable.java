6c2bb0236cd41ad3e3419f3f3feb2dc15b93df10
yashvaidya40 <95998278+yashvaidya40@users.noreply.github.com>
2022-07-14 23:18:16 +0530

    Update Writetable.java

--- src/test/java/TryExcel/TryExcel/Writetable.java
+++ src/test/java/TryExcel/TryExcel/Writetable.java
@@ -24,8 +24,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Writetable {
 
 	public static void main(String[] args) throws IOException {
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\driver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver;
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();   
+		//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\driver\\chromedriver.exe");
+		//WebDriver driver = new ChromeDriver();
 		driver.get("https://www.w3schools.com/html/html_tables.asp");
 		String beforxpath = "//*[@id='customers']//tr[";
 		String[] str = {"yash", "CompanyName","CompanyContact","Country"};
