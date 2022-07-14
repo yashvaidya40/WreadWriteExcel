372a65ce1bdd67e050bab1692f4296253bbbf50f
yashvaidya40 <95998278+yashvaidya40@users.noreply.github.com>
2022-07-14 23:18:59 +0530

    Update Writetable.java

--- src/test/java/TryExcel/TryExcel/Writetable.java
+++ src/test/java/TryExcel/TryExcel/Writetable.java
@@ -15,7 +15,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 //private XSSFSheet sheet = null;
 //int index = workbook.getSheetIndex(sheetName);
